07dfc4ba6f5b256ca61de7a00000cb29670164d1
Emily Drevets <drevets@gmail.com>
2021-09-20 15:41:36 -0500

    server is set up to allow cors

--- server/index.mjs
+++ server/index.mjs
@@ -1,16 +1,17 @@
 import { makeDatabase } from "./utils.mjs";
-import express from "express"
+import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const database = makeDatabase();
-
 
 app.get("/posts", (req, res) => {
   if (process.env.FLAKY) {
     if (Math.random() > 0.85) {
       return res.status(500).json({
-        message: "Internal server error... sorry!"
-      })
+        message: "Internal server error... sorry!",
+      });
     }
   }
   const { page } = req.query;
@@ -19,8 +20,8 @@ app.get("/posts", (req, res) => {
   const slice = database.slice(beginning, beginning + perPage);
   res.json({
     hasNext: beginning + perPage < database.length,
-    posts: slice
-  })
-})
+    posts: slice,
+  });
+});
 
 app.listen(4000, () => console.log("🔌 🎤 We are live on 4000."));
